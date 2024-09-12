bc6333371983a9705fd42265e7e05848c621bd23
EmilianoAprile <emilianoaprile77@icloud.com>
2024-09-12 16:45:15 +0200

    add > can i play right now

--- can-i-play-right-now/app.js
+++ can-i-play-right-now/app.js
@@ -1,2 +1,10 @@
-console.log('Can I play right now?')
+console.log("Can I play right now?");
 
+function canIPlay(now, start, end) {
+  if (start < end) {
+    return now >= start && now < end;
+  }
+  return now >= start || now < end;
+}
+
+console.log(canIPlay(23, 22, 1));
